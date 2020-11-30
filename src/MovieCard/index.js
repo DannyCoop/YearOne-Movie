094b37d39edcd12d1352d419e7bc78cf6eb1a3cf
DannyCoop <Cooperdaniel5141@gmail.com>
2020-11-29 23:37:00 -0500

    went and passed the movie detail page the props

--- src/MovieCard/index.js
+++ src/MovieCard/index.js
@@ -2,8 +2,16 @@ import React from 'react'
 import './MovieCard.css'
 
 function MovieCard(props){
+    const moviePage = () => {
+        return(
+            <moviePage 
+                movie = {props.movie}
+            />
+        )
+    }
+
     return(
-        <div className="MovieCard">
+        <div onClick={() => moviePage()}className="MovieCard">
             <img src={`https://image.tmdb.org/t/p/original${props.movie.poster_path}`}/>
             <h3>{props.movie.title}</h3>
             {console.log(props)}
