eb3692446b4701e8dad2aa125f9c2d2f7a17d2fa
DannyCoop <Cooperdaniel5141@gmail.com>
2020-12-06 14:19:47 -0500

    added folder to hold components

---
+++ src/Components/MovieCard/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './MovieCard.css'
+
+function MovieCard(props){
+    
+
+    return(
+        <div Onclick={() => console.log("hey")}>
+            <img src={`https://image.tmdb.org/t/p/original${props.movie.poster_path}`}/>
+            <h3>{props.movie.title}</h3>
+            {console.log(props)}
+        </div>
+    )
+}
+
+export default MovieCard
