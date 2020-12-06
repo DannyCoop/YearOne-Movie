e1dc53c03583d9e16d0da8daa58615f9a78da972
DannyCoop <Cooperdaniel5141@gmail.com>
2020-12-06 15:48:15 -0500

    made a folder to hold all the container components

---
+++ src/Containers/MovieSearchResults/index.js
@@ -0,0 +1,82 @@
+import React, {useState} from 'react'
+import MovieCard from '../../Components/MovieCard'
+import './MovieSearchResults.css'
+
+function MovieSearchResults() {
+
+    const [searchResult, setSearchResult] = useState("")
+    const [pageNum, setPageNum] = useState('1')
+    const [ movieList, setMoveList] = useState([])
+
+    const handleChange = (e) => {
+        setSearchResult(e.target.value)
+    }
+
+    const ShowMovie = () => {
+        return movieList.map(movie => <MovieCard
+            movie = {movie}
+            // title = {movie.title}
+            // poster = {movie.poster_path}
+            
+        />)
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        fetch("https://api.themoviedb.org/3/search/movie?api_key=c6b6081cc055c927d5cedc00ab4db4b6&language=en-US&query="+ searchResult +"&page="+ pageNum +"&include_adult=false")
+        .then(res => res.json())
+        .then(json => {
+            setMoveList(json.results)
+            setSearchResult("")
+            console.log(movieList)
+        })
+
+    }
+
+    const lastPage = () => {
+        if(pageNum > 1) {
+            setPageNum(pageNum - 1)
+            fetch("https://api.themoviedb.org/3/search/movie?api_key=c6b6081cc055c927d5cedc00ab4db4b6&language=en-US&query="+ searchResult +"&page="+ pageNum +"&include_adult=false")
+            .then(res => res.json())
+            .then(json => {
+                setMoveList(json.results)
+                console.log(movieList)
+            })
+        }
+
+    }
+
+    const nextPage = () => {
+        setPageNum(pageNum + 1)
+        fetch("https://api.themoviedb.org/3/search/movie?api_key=c6b6081cc055c927d5cedc00ab4db4b6&language=en-US&query="+ searchResult +"&page="+ pageNum +"&include_adult=false")
+        .then(res => res.json())
+        .then(json => {
+            setMoveList(json.results)
+            console.log(movieList)
+        })
+    }
+
+    return(
+        <div className="MainContainer">
+            Danny's movie thing!
+           <form onSubmit={(e) => handleSubmit(e)}>
+               <input onChange={(e) => handleChange(e)} name="search" type="text" placeholder="Search"/>
+               <button type="submit">Search</button>
+           </form>
+           <br/>
+           <br/>
+           <br/>
+           <div className="movies-being-shown">
+                {ShowMovie()}
+           </div>
+           <div className="nextPage">
+                <button onClick={() => lastPage()}> Last Page</button>
+                -
+                <button onClick={() => nextPage()}>Next Page</button>
+           </div>
+        </div>
+    )
+}
+
+export default MovieSearchResults
